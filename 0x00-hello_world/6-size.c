da36d83c5524de83b4c54352e6f13d71f5cb0346
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-10 10:37:51 -0800

    Update 6-size.c file--include newline char

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int b;
 	float f;
 
-	printf("Size of a char: %lu byte(s)", sizeof(c));
-	printf("Size of an int: %lu byte(s)", sizeof(i));
-	printf("Size of a long int: %lu byte(s)", sizeof(a));
-	printf("Size of a long long int: %lu byte(s)", sizeof(b));
-	printf("Size of a float: %lu byte(s)", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
 	return (0);
 }
