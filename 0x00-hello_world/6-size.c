ca6e7e0c95a61b49789e4644239a1d53064b0b9b
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-10 10:26:37 -0800

    Type casting

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int a;
+	long long int b;
+	float f;
+
+	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(a));
+	printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(b));
+	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
+
+	return (0);
+}
