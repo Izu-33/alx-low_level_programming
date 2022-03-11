5a92a47a470f40f26bd2fd71810d1029997fcb45
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-11 06:33:11 -0800

    Print alphabet with `putchar` instead of `printf` or otherwise

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
