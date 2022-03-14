5ab535ad4d22cd663bc5bd6a7fdc194a1f8340a8
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-14 08:14:46 -0700

    Conform to Betty coding style

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,11 +17,11 @@ int main(void)
 	{
 		for (b = a + 1; b <= MAX_LIM; b++)
 		{
-			putchar((a/10) + '0');
-			putchar((a%10) + '0');
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
 			putchar(' ');
-			putchar((b/10) + '0');
-			putchar((b%10) + '0');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
 
 			if (a < COMMA_SPACE)
 			{
