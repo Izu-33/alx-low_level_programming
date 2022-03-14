efa033975c61232bd11d9aa3a0a95a468426a285
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-14 08:12:09 -0700

    It's all about getting the algorithm right

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define MAX_LIM 99
+#define COMMA_SPACE 98
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < MAX_LIM; a++)
+	{
+		for (b = a + 1; b <= MAX_LIM; b++)
+		{
+			putchar((a/10) + '0');
+			putchar((a%10) + '0');
+			putchar(' ');
+			putchar((b/10) + '0');
+			putchar((b%10) + '0');
+
+			if (a < COMMA_SPACE)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
