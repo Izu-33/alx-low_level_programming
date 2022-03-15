540cfefb2bd812d66abe4a415b3ee9bae9698873
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-15 08:50:26 -0700

    Check lower case character with user-defined function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lower case character
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
