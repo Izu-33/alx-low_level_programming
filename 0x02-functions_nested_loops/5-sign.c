b7b2c4468769f77f32415a1bc2fcfbc34a039910
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-15 09:23:21 -0700

    Print sign of characters

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @c: character to be checked
+ * Return: 1 if n is greater than zero. 0 if n is zero. -1 otherwise.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
