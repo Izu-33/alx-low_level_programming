d6a28260627daad9e6e18daf03044c83b3207d49
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-15 08:29:21 -0700

    Print alphabet 10x with user-defined function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet
+ * print_alphabet_x10 - Prints 10 times the alphabet in lower case
  */
 void print_alphabet_x10(void)
 {
