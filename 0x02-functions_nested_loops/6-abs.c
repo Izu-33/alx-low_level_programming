2cfc064f75a510f565b9261da2425c0d19c7c3a0
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-15 09:33:17 -0700

    Check for absolute value of character

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @r: character to be checked
+ * Return: r when it is positive. -r otherwise.
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
