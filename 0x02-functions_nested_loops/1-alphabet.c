c30f6e96200946fbe560e8fda92b31ca7b9228f1
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-15 07:53:21 -0700

    Print alphabet with user-defined functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints alphabet in lower case followed by a new line
+ */
+void print_alphabet(void)
+{
+	char c;
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
