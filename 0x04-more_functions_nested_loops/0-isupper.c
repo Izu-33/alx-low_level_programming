19d614979db69e4ceab4faa7030017888c998c05
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-17 08:12:53 -0700

    Commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,20 +3,13 @@
 /**
  * _isupper - Checks for uppercase character
  * @c: character to be checked
- * Retrun: 1 if c is uppercase. Otherwise 0
+ *
+ * Return: 1 if c is uppercase. Otherwise 0
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
-	else if (c <= 'z')
-	{
-		return (0);
-	}
 	else
-	{
 		return (0);
-	}
 }
