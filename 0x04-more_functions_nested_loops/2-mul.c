cab8d73cd8715326a5745ea235b3542684f8a5cf
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-17 02:35:04 -0700

    Multiolication function :heavy_multiplication_x:

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
