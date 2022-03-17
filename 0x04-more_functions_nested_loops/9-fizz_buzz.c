adba38eed04017cc826cfe8a64e0eecb05b1725d
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-17 07:40:31 -0700

    Commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,29 +3,29 @@
 /**
  * main - Prints FizzBuzz instead of integer for integers 
  * divisible by 3, 5, and 15
+ *
+ * Return: Always 0
  */
-void main(void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 15) == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((i % 5) == 0)
-		{
-			printf("Buzz ");
-		}
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
 		else if ((i % 3) == 0)
-		{
-			printf("Fizz ");
-		}
+			printf("Fizz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
 		else
-		{
-			printf("%d ", i);
-		}
+			printf("%d", i);
+
+		if (i == 100)
+			continue;
+		printf(" ");
 	}
+
 	printf("\n");
+	return (0);
 }
