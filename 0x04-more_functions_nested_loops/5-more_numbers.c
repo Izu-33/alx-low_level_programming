fe75079b202a1dc71a98131db187374b8cd442f1
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-17 08:29:17 -0700

    Commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,16 +5,15 @@
  */
 void more_numbers(void)
 {
-	int a;
-	int b;
+	int num, count;
 
-	for (a = 0; a <= 9; a++)
+	for (count = 0; count <= 9; count++)
 	{
-		for (b = 0; b <=14; b++)
+		for (num = 0; num <= 14; num++)
 		{
-			if (b > 9)
-				_putchar((b / 10) + '0');
-			_putchar((b % 10) + '0');
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
 	}
