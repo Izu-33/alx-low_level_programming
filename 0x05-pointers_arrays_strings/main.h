e16b690baec7c8e4e4a5c4e0b7335152fdee6b4d
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-21 16:12:13 -0700

    Commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,5 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, const char *src);
 void print_array(int *a, int n);
