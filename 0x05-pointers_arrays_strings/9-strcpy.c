be49efc0cd9e0ac91d6b76e3ea3f8544f7a2f40a
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-21 15:54:09 -0700

    Commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string
+ * @dest:variable  to copy string to
+ * @src: source to copy to
+ * Return: a pointer
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
