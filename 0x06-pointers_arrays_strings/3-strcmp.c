479b9cd78478093fbfeeba7f5686ef13dbb33ead
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-23 10:37:46 -0700

    Correction from checker

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,14 +8,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int charCompareStatus = 0;
-
-	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
 
-	charCompareStatus = (*s1 == *s2)?0:(*s1 > *s2)?1:-1;
-	return (charCompareStatus);
+	return (*s1 - *s2);
 }
