364c115301100e013f0f44de971b08a0989224a4
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-28 12:09:52 -0700

    Update `memset` function

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,6 +5,7 @@
  * @s: pointer to variable n
  * @b: value to be filled
  * @n: number of bytes to be filled starting from s
+ *
  * Return: a pointer to the memory area of variable s
  */
 char *_memset(char *s, char b, unsigned int n)
