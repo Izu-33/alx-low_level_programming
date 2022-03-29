224957e31570f9a6c6ed3bdd8c65823188a490ba
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-29 03:36:25 -0700

    Update `strchr` function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,9 +10,9 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; *(s + 1) >= '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if ((*(s + 1)) == c)
+		if (s[i] == c)
 			return (s + i);
 	}
 
