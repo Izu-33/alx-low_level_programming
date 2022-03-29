86b26682f8f746649965ad68f43609a56c4a6de1
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-29 04:05:31 -0700

    Update `strspn` function

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,12 +15,15 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (i = 0; accept[i]; i++)
 		{
-			bytes++;
-			break;
-		}
-		else if (accept[i + 1] == '\0')
-		{
-			return (bytes);
+			if (*s == accept[i])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+			{
+				return (bytes);
+			}
 		}
 
 		s++;
