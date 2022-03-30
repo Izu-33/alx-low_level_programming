3a78bc968fdb7f7000cb2298c4bee38750b86304
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-03-30 04:56:04 -0700

    Writing recursive functions and updating `main.h`

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - prints a string followed by a newline.
+ * @s: string to be printed
+ */
 void _puts_recursion(char *s)
 {
 	if (*s)
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string to be checked.
+ * Return: the length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+	len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+
+	return (len);
+}
