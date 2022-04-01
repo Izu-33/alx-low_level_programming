abf9c0986d9b3a0fcea5834f6c40b20548c7fac7
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-04-01 07:20:29 -0700

    Conform to `Betty`

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,8 +2,8 @@
 
 /**
  * main - prints the name of the program, and a new line.
- * argc: the number of arguments supplied to the program.
- * argv: an array of pointers to the arguments.
+ * @argc: the number of arguments supplied to the program.
+ * @argv: an array of pointers to the arguments.
  * Return: 0 always.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
