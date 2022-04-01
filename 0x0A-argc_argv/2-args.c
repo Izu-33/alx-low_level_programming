8b714fc9ed60eb60b2abd0b0fe41cff556f9010d
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-04-01 07:39:55 -0700

    Printing out all arguments passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: count of all arguments passed.
+ * @argv: array of pointers to the arguments.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	for (;count < argc; count++)
+		printf("%s\n", argv[count]);
+
+	return (0);
+}
