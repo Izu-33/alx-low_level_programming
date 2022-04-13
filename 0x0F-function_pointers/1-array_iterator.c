b2358a159ad2f1d934d689cb9c48c0e749ebe2b4
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-04-13 13:15:11 -0700

    Correct `1-array_iterator.c`

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 
-	int i;
-
-	for (i = 0; i < size; i++)
+	while (size-- > 0)
 	{
 		action(*array);
 		array++;
