0a1879f5a403c561fd5768d1fc1b2acb0b825512
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-04-13 13:40:29 -0700

    Searching an array parameter with another function pointer parameter

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an integer.
+ * @array: Array of ints.
+ * @size: Size of the array.
+ * @cmp: A pointer to the compaare function.
+ *
+ * Return: -1 if no element matches or size <= 0.
+ * Otherwise, the index of the 1st element for which
+ * the compare function does not return 0.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
