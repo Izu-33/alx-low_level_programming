385acaa0284fba3ad30f681546ee7e2543fea0cd
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-04-13 13:08:58 -0700

    Passing mor functions together  with arrays as arguments

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function given as a parameter
+ * on each element of an array.
+ * @array: Array passed as argument.
+ * @size: Size of @array.
+ * @action: Function to be executed passed as argument.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+
+}
