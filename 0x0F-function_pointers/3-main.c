3f1e79f9c736132437dccf19e5453690caf9417b
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-04-13 17:00:51 -0700

    Update `3-main.c` file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,20 +24,20 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	operator = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) ||
-			(*op == '%' && num2 == 0))
+	if ((*operator == '/' && num2 == 0) ||
+			(*operator == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n", get_op_func(operator)(num1, num2));
 
 	return (0);
 }
