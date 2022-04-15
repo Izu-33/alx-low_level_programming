895b3bb174564625263c60ddae8e1078557474fb
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-04-14 21:18:25 -0700

    Change square bracket notation pointer notation

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -70,10 +70,10 @@ void print_all(const char * const format, ...)
 
 	va_start(args, format);
 
-	while (format && format[i])
+	while (format && (*(format + i)))
 	{
 		j = 0;
-		while (j < 4 && format[i] != funcs[j].c)
+		while (j < 4 && (*(format + i) != *(funcs[j].c)))
 		{
 			j++;
 		}
