3d61f211a8514c081f4640d2d8d17735e094d1ee
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-04-14 20:07:08 -0700

    Update initialization segment of `for` loop statement

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(strings, n);
 
-	for (i = n; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(strings, char *);
 
