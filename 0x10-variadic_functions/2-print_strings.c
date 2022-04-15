6ad9f59fc36f07b030d7ab062e9b2ac163ff3095
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-04-14 20:04:16 -0700

    Update second `if` statement

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", str);
 
-		if (i < (n - 1) && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
