35dc60a523e5e8699fc5f0f91df399c6b72f3bcd
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-04-27 16:00:34 -0700

    Added files 8 to 10

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Calculates the sum of all the
+ * data of a listint_t list.
+ * @head: A pointer to the head of the listint_t list.
+ * Return: 0 if the list is empty. Otherwise,
+ * the sum of all the data.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
