3b50048226bbe1c02edba657a54fd24ab474a138
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-04-27 16:08:48 -0700

    Error correction for file 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (node = 0; node < (index -. 1); node++)
+	for (node = 0; node < (index - 1); node++)
 	{
 		if (copy->next == NULL)
 			return (-1);
