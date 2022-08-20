a4fcbab14747924aa641f55470b45c0c1fa28c5d
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-08-20 04:37:15 -0700

    Update island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,14 +15,13 @@ def island_perimeter(grid):
     row_num = len(grid)
     col_num = len(grid[0])
     islands, overlaps = 0, 0
-    if row_num == 0 || col_num == 0:
-        return 0
+
     for i in range(row_num):
         for j in range(col_num):
             if grid[i][j] == 1:
                 islands += 1
-                if i < row_num - 1 && grid[i + 1][j] == 1:
+                if (i > 0 and grid[i - 1][j] == 1):
                     overlaps += 1
-                if j < col_num - 1 && grid[i][j + 1] == 1:
+                if (j > 0 and grid[i][j - 1] == 1):
                     overlaps += 1
     return islands * 4 - overlaps * 2
